4e972979834c3527a4fd03ecadbdbef44d483e03
7777777derek <87050285+7777777derek@users.noreply.github.com>
2023-05-04 12:15:26 -0400

    haha

---
+++ Final copy/sketch.js
@@ -0,0 +1,93 @@
+//code: myself+chatGPT
+//music: myself
+//inspiration: Dan Shiffman: https://www.youtube.com/watch?v=H81Tdrmz2LA
+
+/*let video;
+let seriously;
+let blurEffect;
+let source;
+let target;
+let canvas;
+let objectDetector;
+
+function setup() {
+  canvas = createCanvas(640, 480,WEBGL);
+  video = createCapture(VIDEO);
+  video.size(width, height);
+  video.hide();
+
+  objectDetector = ml5.objectDetector('cocossd', modelReady);
+
+  seriously = new Seriously();
+  source = seriously.source(video.elt);
+  target = seriously.target(canvas.elt);
+  blurEffect = seriously.effect('blur');
+  blurEffect.source = source;
+  target.source = blurEffect;
+  seriously.go();
+}
+
+function modelReady() {
+  console.log('Model is ready!');
+  objectDetector.detect(video, gotResults);
+}
+
+function gotResults(error, results) {
+  if (error) {
+    console.error(error);
+  } else {
+    let detectionLength = results.length;
+    blurEffect.amount = map(detectionLength, 0, 5, 1, 0);
+    objectDetector.detect(video, gotResults);
+  }
+}*/
+let video;
+let seriously;
+let effects = ['blur', 'noise', 'polar', 'colorcube','sepia','vignette','hue-saturation'];
+let currentEffect;
+let source;
+let target;
+let canvas;
+let objectDetector;
+
+function setup() {
+  canvas = createCanvas(640, 480, WEBGL);
+  video = createCapture(VIDEO);
+  video.size(width, height);
+  video.hide();
+
+  objectDetector = ml5.objectDetector('cocossd', modelReady);
+
+  seriously = new Seriously();
+  source = seriously.source(video.elt);
+  target = seriously.target(canvas.elt);
+  seriously.go();
+}
+
+function modelReady() {
+  console.log('Model is ready!');
+  objectDetector.detect(video, gotResults);
+}
+
+function gotResults(error, results) {
+  if (error) {
+    console.error(error);
+  } else {
+    let detectionLength = results.length;
+    updateEffect(detectionLength);
+    objectDetector.detect(video, gotResults);
+  }
+}
+
+function updateEffect(detectionLength) {
+  let effectIndex = detectionLength % effects.length;
+  let effectName = effects[effectIndex];
+
+  if (currentEffect) {
+    currentEffect.destroy();
+  }
+
+  currentEffect = seriously.effect(effectName);
+  currentEffect.source = source;
+  target.source = currentEffect;
+}
